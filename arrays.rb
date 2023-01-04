d19ddd3f5d75f8ab1ed972f452369f9f4757193b
Crow Rising <miscbyrd@gmail.com>
2023-01-04 16:34:14 -0700

    Add syntax to arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,19 @@ days = [Monday, Wednesday, Friday, Sunday]
 hours = [3, 6, 8, 1,]
 amount = [5.5, 7.5. 100.25, 8.75]
 due = [true, true, false, true]
+
+# With pop the last day (sunday) will be removed. 
+## Push should return the last element but I never took it away so I am not sure the point of this.
+### Shift will remove the float in the 0 space
+#### Unshift returns the 1st element, but I believe it means the 0 space.
+days.pop
+p days
+
+hours.push
+p hours
+
+amount.shift
+p shift
+
+due.unshift
+p due
